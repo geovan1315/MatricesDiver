5f1c0619523d4e62fa43f6bec3b21aaa653823c3
geovanni valladares <cvalladares@juanaparicio.edu.gt>
2020-06-04 12:45:21 -0600

    Aplicacion de matriz

--- Matrices/Matrices/Program.cs
+++ Matrices/Matrices/Program.cs
@@ -8,8 +8,32 @@ namespace Matrices
 {
     class Program
     {
+        static int val = 3;
+        static int[,] numeros = new int[val, val];//3X3
         static void Main(string[] args)
         {
+            string linea;
+            for (int f=0;f<val;f++)
+            {
+                for (int c = 0; c < val; c++)
+                {
+                    Console.WriteLine("f: "+f+"  c: "+c+"\n\n");
+                    Console.Write("Ingrese un valor: ");
+                    linea = Console.ReadLine();
+                    numeros[c, f] = int.Parse(linea);
+                }
+            }
+            Console.WriteLine("\n----------------------------------\n");
+            for (int f = 0; f < val; f++)
+            {
+                for (int c = 0; c < val; c++)
+                {
+                    Console.WriteLine("Valor: "+numeros[c,f]);
+                    
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
